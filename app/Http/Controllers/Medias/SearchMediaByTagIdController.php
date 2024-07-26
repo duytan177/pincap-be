bea79073e981c61c4b027c6b848c46e0c719c151
Lê Duy Tân <tanld@d-soft.com.vn>
2024-07-26 22:31:13 +0700

    fix : add parameter is created for my media

---
+++ app/Http/Controllers/Medias/SearchMediaByTagIdController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Medias;
+
+use App\Enums\Album_Media\Privacy;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Medias\SearchMediaRequest;
+use App\Http\Resources\Medias\Media\MediaCollection;
+use App\Models\Media;
+
+class SearchMediaByTagIdController extends Controller
+{
+    public function __invoke($tagId, SearchMediaRequest $request)
+    {
+        $perPage = $request->input('per_page');
+        $page = $request->input("page");
+
+        $medias = Media::where([
+            ["is_created", "=", true],
+            ["privacy", "=", Privacy::PUBLIC]
+        ])->whereHas('tags', function ($query) use ($tagId) {
+            $query->where('tag_id', $tagId);
+        })->paginate($perPage, ['*'], 'page', $page);
+
+        return new MediaCollection($medias);
+    }
+}
