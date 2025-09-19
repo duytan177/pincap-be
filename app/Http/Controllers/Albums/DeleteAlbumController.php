4c6e0d66ab02abaf8e8980f372b577d7ecfcfb44
Lê Duy Tân <tanld@d-soft.com.vn>
2025-09-19 18:18:55 +0700

    refactor: remove media from album and remove delete album check permission

--- app/Http/Controllers/Albums/DeleteAlbumController.php
+++ app/Http/Controllers/Albums/DeleteAlbumController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers\Albums;
 
 use App\Http\Controllers\Controller;
 use App\Models\Album;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Exceptions\Albums\AlbumException;
 
 class DeleteAlbumController extends Controller
 {
     public function __invoke($albumId)
     {
-        Album::findOrFail($albumId)->delete();
+        $currentUserId = JWTAuth::user()->getAttribute("id");
+
+        $isOwner = Album::query()->where('id', $albumId)->ownedBy($currentUserId)->exists();
+
+        if (!$isOwner) {
+            throw AlbumException::notOwner();
+        }
+
+        $deleted = Album::where("id", $albumId)->delete();
+        if ($deleted === 0) {
+            throw AlbumException::deleteFailed();
+        }
 
         return responseWithMessage("Deleted album successfully");
     }
