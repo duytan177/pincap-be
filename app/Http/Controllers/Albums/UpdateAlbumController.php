0abe2279bf3b811ca31d7ca4b8ba3c774cdf5d6d
Lê Duy Tân <tanld@d-soft.com.vn>
2025-10-17 20:01:10 +0700

    feat: update album controller to include permission checks for album updates based on user role and invitation status

--- app/Http/Controllers/Albums/UpdateAlbumController.php
+++ app/Http/Controllers/Albums/UpdateAlbumController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Albums;
 
+use App\Enums\Album_Media\AlbumRole;
+use App\Enums\Album_Media\InvitationStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Albums\AlbumRequest;
 use App\Models\Album;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateAlbumController extends Controller
 {
@@ -12,8 +15,8 @@ class UpdateAlbumController extends Controller
     public function __invoke($albumId, AlbumRequest $request)
     {
         $dataAlbum = $request->validated();
-
-        $album = Album::findOrFail($albumId);
+        $userId = Auth::user()->id;
+        $album = Album::findOrFailWithPermission($albumId, $userId, [AlbumRole::OWNER], [InvitationStatus::ACCEPTED]);
 
         $album->update($dataAlbum);
 
