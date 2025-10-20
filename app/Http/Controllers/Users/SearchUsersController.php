a2262853b367a8aad868415b61944fa0ffb5f6d2
Lê Duy Tân <tanld@d-soft.com.vn>
2025-10-20 15:53:47 +0700

    feat: add condition to exclude soft-deleted user_album records in SearchUsersController

--- app/Http/Controllers/Users/SearchUsersController.php
+++ app/Http/Controllers/Users/SearchUsersController.php
@@ -33,7 +33,8 @@ class SearchUsersController extends Controller
             ->when(!empty($albumId), function ($query) use ($albumId) {
                 $query->leftJoin('user_album as ua', function ($join) use ($albumId) {
                     $join->on('users.id', '=', 'ua.user_id')
-                        ->where('ua.album_id', '=', $albumId);
+                        ->where('ua.album_id', '=', $albumId)
+                        ->whereNull('ua.deleted_at');
                 })
                     ->addSelect('users.*', 'ua.invitation_status as status');
             })
